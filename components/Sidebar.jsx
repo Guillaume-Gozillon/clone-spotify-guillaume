04937671b7721a21fdf70529b73ecca506356ec3
Guillaume <guillaumegozillon@gmail.com>
2022-02-03 21:56:54 +0100

    Start project

---
+++ components/Sidebar.jsx
@@ -0,0 +1,31 @@
+import {
+  HomeIcon,
+  SearchIcon,
+  LibraryIcon,
+  PlusCircleIcon
+} from '@heroicons/react/outline'
+
+function Sidebar() {
+  return (
+    <div>
+      <button>
+        <HomeIcon className='h-5 w-5' />
+        <p>Home</p>
+      </button>
+      <button>
+        <SearchIcon className='h-5 w-5' />
+        <p>Search</p>
+      </button>
+      <button>
+        <LibraryIcon className='h-5 w-5' />
+        <p>Your library</p>
+      </button>
+      <button>
+        <PlusCircleIcon className='h-5 w-5' />
+        <p>Home</p>
+      </button>
+    </div>
+  )
+}
+
+export default Sidebar
