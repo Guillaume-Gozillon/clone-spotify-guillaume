b1104755e74dc83537befade8be73bf39d649304
Guillaume <guillaumegozillon@gmail.com>
2022-02-05 01:47:45 +0100

    Add: Player music

--- components/Center.jsx
+++ components/Center.jsx
@@ -1,29 +1,40 @@
-import { useSession } from 'next-auth/react'
+import { signOut, useSession } from 'next-auth/react'
 import { ChevronDownIcon } from '@heroicons/react/outline'
 import { useEffect, useState } from 'react'
 import { shuffle } from 'lodash'
+import { useRecoilState, useRecoilValue } from 'recoil'
+import { playlistIdState, playlistState } from '../atoms/playlistAtom'
+import useSpotify from '../hooks/useSpotify'
+import { colors } from '../utils'
+import Songs from './Songs'
 
 function Center() {
-  const colors = [
-    'from-indigo-500',
-    'from-blue-500',
-    'from-green-500',
-    'from-red-500',
-    'from-yellow-500',
-    'from-pink-500',
-    'from-purple-500'
-  ]
   const { data: session } = useSession()
   const [color, setColor] = useState(null)
+  const spotifyApi = useSpotify()
+  const playlistsId = useRecoilValue(playlistIdState)
+  const [playlist, setPlaylist] = useRecoilState(playlistState)
 
   useEffect(() => {
     setColor(shuffle(colors).pop())
-  }, [])
+  }, [playlistsId])
+
+  useEffect(() => {
+    spotifyApi
+      .getPlaylist(playlistsId)
+      .then(data => {
+        setPlaylist(data.body)
+      })
+      .catch(err => console.log('Something went wrong', err))
+  }, [spotifyApi, playlistsId])
 
   return (
-    <div className='flex-grow'>
+    <div className='flex-grow h-screen overflow-y-scroll scrollbar-hide'>
       <header className='absolute top-5 right-8'>
-        <div className='flex items-center bg-red-300 space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2'>
+        <div
+          className='flex items-center bg-black text-white space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2'
+          onClick={signOut}
+        >
           <img
             className='rounded-full w-10 h-10'
             src={session?.user.image}
@@ -34,11 +45,23 @@ function Center() {
         </div>
       </header>
       <section
-        className={`flex items-end space-x-7 bg-gradient-to-b to-black ${color} h-80 text-white padding-8`}
+        className={`flex items-end space-x-7 bg-gradient-to-b to-black ${color} h-80 text-white p-8`}
       >
-        <h1>HEYYY</h1>
-        {/* <img src="" alt="" /> */}
+        <img
+          className='h-44 w-44 shadow-2xl'
+          src={playlist?.images?.[0].url}
+          alt=''
+        />
+        <div>
+          <p>Playlist</p>
+          <h1 className='text-2xl md:text-3xl xl:text-5xl font-bold'>
+            {playlist?.name}
+          </h1>
+        </div>
       </section>
+      <div>
+        <Songs />
+      </div>
     </div>
   )
 }
--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -8,12 +8,15 @@ import {
 } from '@heroicons/react/outline'
 import { signOut, useSession } from 'next-auth/react'
 import { useEffect, useState } from 'react'
+import { useRecoilState } from 'recoil'
 import useSpotify from '../hooks/useSpotify'
+import { playlistIdState } from '../atoms/playlistAtom'
 
 function Sidebar() {
   const spotifyApi = useSpotify()
-  const { data: session, status } = useSession()
+  const { data: session } = useSession()
   const [playlists, setPlaylists] = useState([])
+  const [playlistsId, setPlaylistsId] = useRecoilState(playlistIdState)
 
   useEffect(() => {
     if (spotifyApi.getAccessToken()) {
@@ -23,17 +26,9 @@ function Sidebar() {
     }
   }, [session, spotifyApi])
 
-  console.log(session)
-  console.log(playlists)
   return (
-    <div className='text-gray-500 p-5 text-sm border-r border-gray-900 overflow-y-scroll scrollbar-hide h-screen'>
+    <div className='text-gray-500 p-5 text-xs lg:text-sm border-r border-gray-900 overflow-y-scroll scrollbar-hide h-screen sm:max-w-[12rem] lg:max-w-[15rem hidden md:inline-flex pb-36'>
       <div className='space-y-4'>
-        <button
-          className='flex items-center space-x-2 hover:text-white'
-          onClick={() => signOut()}
-        >
-          <p>Se déconnecter</p>
-        </button>
         <button className='flex items-center space-x-2 hover:text-white'>
           <HomeIcon className='h-5 w-5' />
           <p>Accueil</p>
@@ -64,7 +59,11 @@ function Sidebar() {
         <hr className='border-t-[0.1px] border-gray-900' />
 
         {playlists.map((playlist, i) => (
-          <p className='cursor-pointer hover:text-white' key={i}>
+          <p
+            className='cursor-pointer hover:text-white'
+            key={i}
+            onClick={() => setPlaylistsId(playlist.id)}
+          >
             {playlist.name}
           </p>
         ))}
